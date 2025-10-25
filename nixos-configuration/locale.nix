807f6ef894d9e143b6662113641aa56172a40e49
Tommaso Acciarresi <49035039+tomfrost435@users.noreply.github.com>
2025-10-25 05:37:26 +0200

    modified:   nixos-configuration/locale.nix

--- nixos-configuration/locale.nix
+++ nixos-configuration/locale.nix
@@ -2,9 +2,11 @@
   time.timeZone = "Europe/Rome";
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
+    LANGUAGE = "en_US.UTF-8";
     LC_ADDRESS = "it_IT.UTF-8";
     LC_COLLATE = "it_IT.UTF-8";
     LC_CTYPE = "it_IT.UTF8";
+    LC_IDENTIFICATION = "it_IT.UTF-8";
     LC_MEASUREMENT = "it_IT.UTF-8";
     LC_MESSAGES = "it_IT.UTF-8";
     LC_MONETARY = "it_IT.UTF-8";
